cfd53e6c3ce496acac8316980d10c4a32f7dad7c
MarcosPaul0 <marcosphip7@gmail.com>
2026-03-05 21:49:44 -0300

    fix: acessibility link

--- src/pages/store/components/whatsapp-button.tsx
+++ src/pages/store/components/whatsapp-button.tsx
@@ -1,14 +1,15 @@
-import { URL_WHATSAPP } from '@autoria/constants/urls'
-import { WhatsappLogoIcon } from '@phosphor-icons/react/dist/ssr'
+import { URL_WHATSAPP } from "@autoria/constants/urls";
+import { WhatsappLogoIcon } from "@phosphor-icons/react/dist/ssr";
 
 export function WhatsappButton() {
-  return (
-    <a
-      href={URL_WHATSAPP}
-      target="_blank"
-      className="p-4 bg-whatsapp hover:brightness-110 duration-300 text-whatsapp-foreground fixed bottom-8 right-8 rounded-full"
-    >
-      <WhatsappLogoIcon className="size-8" />
-    </a>
-  )
+	return (
+		<a
+			href={URL_WHATSAPP}
+			target="_blank"
+			className="p-4 bg-whatsapp hover:brightness-110 duration-300 text-whatsapp-foreground fixed bottom-8 right-8 rounded-full"
+			aria-label="Entre em contato pelo Whatsapp"
+		>
+			<WhatsappLogoIcon className="size-8" />
+		</a>
+	);
 }
