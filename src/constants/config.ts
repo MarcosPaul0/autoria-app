125af1d9b49bab04a0b5af7e0c84599f1eca3372
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-01 20:49:17 -0300

    feat: adiciona nova variável de configuração

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,7 @@
 export const ITEMS_PER_PAGE = Number(
-  import.meta.env.VITE_DEFAULT_ITEMS_PER_PAGE,
-)
+	import.meta.env.VITE_DEFAULT_ITEMS_PER_PAGE,
+);
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+export const AUTH_TOKEN_COOKIE = import.meta.env.VITE_AUTH_TOKEN_COOKIE;
