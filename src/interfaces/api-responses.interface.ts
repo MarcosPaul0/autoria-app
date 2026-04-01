1b4feab34fa8b141c5f01c74ab59977b8fb1f245
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-01 20:49:44 -0300

    refactor: atualiza interfaces de entidades da api

--- src/interfaces/api-responses.interface.ts
+++ src/interfaces/api-responses.interface.ts
@@ -1,61 +1,56 @@
 export interface Pagination<T> {
-  items: Array<T>
-  totalItems: number
-  totalPages: number
-  page: number
-  hasNext: boolean
-  hasPrevious: boolean
-  itemsPerPage: number
+	items: Array<T>;
+	totalItems: number;
+	totalPages: number;
+	page: number;
+	hasNext: boolean;
+	hasPrevious: boolean;
+	itemsPerPage: number;
 }
 
 export interface Entity {
-  id: string
-  createdAt: string
-  updatedAt: string
+	id: string;
+	createdAt: string;
+	updatedAt: string;
 }
 
 export interface ProductImage extends Entity {
-  imageUrl: string
-  displayOrder: number
-}
-
-export interface ProductByAdmin extends Entity {
-  name: string
-  printDescription: string
-  description: string
-  priceInCents: number
-  productionTimeInMinutes: number
-  discountPercentage: number
-  isActive: boolean
-  stockQuantity: number
-  productCategoryId: string
-  category: string
-  productImages: Array<ProductImage>
+	imageUrl: string;
+	displayOrder: number;
 }
 
 export interface Product extends Entity {
-  id: string
-  name: string
-  printDescription: string
-  description: string
-  priceInCents: number
-  discountPercentage: number
-  productCategoryId: string
-  category: string
-  productImages: Array<ProductImage>
+	name: string;
+	printDescription: string;
+	description: string;
+	priceInCents: number;
+	discountPercentage: number;
+	productCategoryId: string;
+	category: string;
+	productImages: Array<ProductImage>;
+}
+
+export interface ProductForAdmin extends Product {
+	productionTimeInMinutes: number;
+	isActive: boolean;
+	stockQuantity: number;
 }
 
 export interface ProductItem {
-  id: string
-  name: string
-  priceInCents: number
-  discountPercentage: number
-  productCategoryId: string
-  category: string
-  productImage: ProductImage
+	id: string;
+	name: string;
+	priceInCents: number;
+	discountPercentage: number;
+	productCategoryId: string;
+	category: string;
+	productImage: ProductImage;
 }
 
 export interface ProductCategory extends Entity {
-  category: string
-  productCount: number
+	category: string;
+}
+
+export interface ProductCategoryForAdmin extends ProductCategory {
+	isActive: boolean;
+	productCount: number;
 }
