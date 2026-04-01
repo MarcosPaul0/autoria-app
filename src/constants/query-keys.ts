956a39b080fab6467d9cf87079791a09177d5cfc
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-01 20:55:23 -0300

    refactor: atualiza chave de query

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,6 +1,7 @@
 export const QUERY_KEYS = {
-  productCategories: 'product-categories',
-  productCategoryOptions: 'product-categories-options',
-  products: 'products',
-  productsForAdmin: 'product-for-admin',
-} as const
+	productCategories: "product-categories",
+	productCategoriesForAdmin: "product-categories-for-admin",
+	productCategoryOptions: "product-categories-options",
+	products: "products",
+	productsForAdmin: "product-for-admin",
+} as const;
