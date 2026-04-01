65cadb44677a6a3b7549e59e801893fe54305b2d
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-01 20:55:01 -0300

    feat: adiciona ativação de categoria

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,67 +1,70 @@
+import { LinkButton } from "@autoria/components/link-button";
 import {
-  Table,
-  TableBody,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@autoria/components/table'
-import { Toolbar } from '@autoria/components/toolbar'
-import { LinkButton } from '@autoria/components/link-button'
-import { APP_ROUTE } from '@autoria/constants/app-route'
-import { PlusIcon } from '@phosphor-icons/react'
-import { useQuery } from '@tanstack/react-query'
-import { ProductCategoryRepository } from '@autoria/repositories/product-category-repository'
-import { ProductCategoryPresenter } from '@autoria/presenters/product-category-presenter'
-import { QUERY_KEYS } from '@autoria/constants/query-keys'
-import { CategoryItem } from './components/category-item'
+	Table,
+	TableBody,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@autoria/components/table";
+import { Toolbar } from "@autoria/components/toolbar";
+import { APP_ROUTE } from "@autoria/constants/app-route";
+import { QUERY_KEYS } from "@autoria/constants/query-keys";
+import { productCategoryForAdminListToUi } from "@autoria/presenters/product-category-presenter";
+import { listAllProductCategoriesForAdmin } from "@autoria/repositories/product-category-repository";
+import { PlusIcon } from "@phosphor-icons/react";
+import { useQuery } from "@tanstack/react-query";
+import { ProductCategoryItem } from "./components/product-category-item";
 
 export function CategoriesPage() {
-  const { data: productCategoriesData } = useQuery({
-    queryKey: [QUERY_KEYS.productCategories],
-    queryFn: async () => {
-      const productCategoriesResponse =
-        await ProductCategoryRepository.listAll()
+	const { data: productCategoriesData } = useQuery({
+		queryKey: [QUERY_KEYS.productCategoriesForAdmin],
+		queryFn: async () => {
+			const productCategoriesResponse =
+				await listAllProductCategoriesForAdmin();
 
-      return ProductCategoryPresenter.listToUi(productCategoriesResponse)
-    },
-  })
+			return productCategoryForAdminListToUi(productCategoriesResponse);
+		},
+	});
 
-  return (
-    <>
-      <Toolbar page="Lista de categorias">
-        <LinkButton to={APP_ROUTE.private.addCategory}>
-          <PlusIcon />
-          Nova categoria
-        </LinkButton>
-      </Toolbar>
+	return (
+		<>
+			<Toolbar page="Lista de categorias">
+				<LinkButton to={APP_ROUTE.private.addCategory}>
+					<PlusIcon />
+					Nova categoria
+				</LinkButton>
+			</Toolbar>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Categoria</TableHead>
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Categoria</TableHead>
 
-            <TableHead>Produtos</TableHead>
+						<TableHead>Produtos</TableHead>
 
-            <TableHead>Registro</TableHead>
+						<TableHead>Status</TableHead>
 
-            <TableHead className="w-[100px] text-center">Ações</TableHead>
-          </TableRow>
-        </TableHeader>
+						<TableHead>Registro</TableHead>
 
-        <TableBody>
-          {productCategoriesData?.map((productCategory) => (
-            <CategoryItem
-              key={productCategory.id}
-              productCategory={{
-                id: productCategory.id,
-                category: productCategory.category,
-                productsCount: productCategory.productCount,
-                createdAt: productCategory.createdAt,
-              }}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </>
-  )
+						<TableHead className="w-[100px] text-center">Ações</TableHead>
+					</TableRow>
+				</TableHeader>
+
+				<TableBody>
+					{productCategoriesData?.map((productCategory) => (
+						<ProductCategoryItem
+							key={productCategory.id}
+							productCategory={{
+								id: productCategory.id,
+								category: productCategory.category,
+								isActive: productCategory.isActive,
+								productsCount: productCategory.productCount,
+								createdAt: productCategory.createdAt,
+							}}
+						/>
+					))}
+				</TableBody>
+			</Table>
+		</>
+	);
 }
--- src/pages/categories/update.tsx
+++ src/pages/categories/update.tsx
@@ -1,115 +1,127 @@
+import { Button } from "@autoria/components/button";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@autoria/components/card'
-import { Toolbar } from '@autoria/components/toolbar'
-import { APP_ROUTE } from '@autoria/constants/app-route'
-import { ToastService } from '@autoria/services/toast-service'
-import { useForm, useStore } from '@tanstack/react-form'
-import * as z from 'zod'
-import { Button } from '@autoria/components/button'
-import { HTTP_STATUS } from '@autoria/constants/http-status'
-import { errorHandler } from '@autoria/utils/errorHandler'
-import { apiClient } from '@autoria/services/api-service'
-import { API_ROUTES } from '@autoria/constants/api-routes'
-import { useQuery } from '@tanstack/react-query'
-import { ProductCategoryRepository } from '@autoria/repositories/product-category-repository'
-import { FormatterHelper } from '@autoria/helpers/formatter-helper'
-import { FormInput } from '@autoria/components/form-input'
-import type { HttpStatus } from '@autoria/constants/http-status'
-import type { SubmitEvent } from 'react'
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+} from "@autoria/components/card";
+import { FormInput } from "@autoria/components/form-input";
+import { FormSwitch } from "@autoria/components/form-switch";
+import { Toolbar } from "@autoria/components/toolbar";
+import { API_ROUTES } from "@autoria/constants/api-routes";
+import { APP_ROUTE } from "@autoria/constants/app-route";
+import type { HttpStatus } from "@autoria/constants/http-status";
+import { HTTP_STATUS } from "@autoria/constants/http-status";
+import { QUERY_KEYS } from "@autoria/constants/query-keys";
+import { FormatterHelper } from "@autoria/helpers/formatter-helper";
+import { findProductCategoryByIdForAdmin } from "@autoria/repositories/product-category-repository";
+import { apiClient } from "@autoria/services/api-service";
+import { ToastService } from "@autoria/services/toast-service";
+import { errorHandler } from "@autoria/utils/errorHandler";
+import { useForm, useStore } from "@tanstack/react-form";
+import { useQuery } from "@tanstack/react-query";
+import type { SubmitEvent } from "react";
+import * as z from "zod";
 
 const updateCategoryFormSchema = z.object({
-  category: z
-    .string()
-    .min(10, 'Categoria deve ter no mínimo 10 caracteres')
-    .max(100, 'Categoria deve ter no máximo 100 caracteres'),
-})
+	category: z
+		.string()
+		.min(5, "Categoria deve ter no mínimo 5 caracteres")
+		.max(100, "Categoria deve ter no máximo 100 caracteres"),
+	isActive: z.boolean(),
+});
 
 const UPDATE_PRODUCT_CATEGORY_SUCCESS_MESSAGE =
-  'Nova coleção registrada com sucesso!'
+	"Nova coleção registrada com sucesso!";
 
 const UPDATE_PRODUCT_CATEGORY_ERROR_MESSAGES = {
-  [HTTP_STATUS.unauthorized]: 'Ocorreu um erro ao atualizar a categoria!',
-  [HTTP_STATUS.badRequest]: 'Ocorreu um erro ao atualizar a categoria!',
-  [HTTP_STATUS.internal]:
-    'Ocorreu algum erro inesperado. Tente novamente mais tarde.',
-} as Record<HttpStatus, string>
+	[HTTP_STATUS.unauthorized]: "Ocorreu um erro ao atualizar a categoria!",
+	[HTTP_STATUS.badRequest]: "Ocorreu um erro ao atualizar a categoria!",
+	[HTTP_STATUS.internal]:
+		"Ocorreu algum erro inesperado. Tente novamente mais tarde.",
+} as Record<HttpStatus, string>;
 
 interface UpdateCategoryPageProps {
-  categoryId: string
+	categoryId: string;
 }
 
 export function UpdateCategoryPage({ categoryId }: UpdateCategoryPageProps) {
-  const { data: productCategoryData } = useQuery({
-    queryKey: ['product-category', categoryId],
-    queryFn: async () => await ProductCategoryRepository.findById(categoryId),
-  })
+	const { data: productCategoryData } = useQuery({
+		queryKey: [QUERY_KEYS.productCategoriesForAdmin, categoryId],
+		queryFn: async () => await findProductCategoryByIdForAdmin(categoryId),
+	});
 
-  const updateCategoryForm = useForm({
-    defaultValues: {
-      category: FormatterHelper.stringToString(productCategoryData?.category),
-    },
-    validators: {
-      onSubmit: updateCategoryFormSchema,
-    },
-    onSubmit: async ({ value }) => {
-      try {
-        await apiClient.patch(
-          `${API_ROUTES.productCategory.update}${categoryId}`,
-          {
-            category: value.category,
-          },
-        )
+	const updateCategoryForm = useForm({
+		defaultValues: {
+			category: FormatterHelper.stringToString(productCategoryData?.category),
+			isActive: productCategoryData?.isActive,
+		},
+		validators: {
+			onSubmit: updateCategoryFormSchema,
+		},
+		onSubmit: async ({ value }) => {
+			try {
+				await apiClient.patch(
+					`${API_ROUTES.productCategory.update}${categoryId}`,
+					{
+						category: value.category,
+						isActive: value.isActive,
+					},
+				);
 
-        ToastService.success(UPDATE_PRODUCT_CATEGORY_SUCCESS_MESSAGE)
-      } catch (error) {
-        errorHandler(error, UPDATE_PRODUCT_CATEGORY_ERROR_MESSAGES)
-      }
-    },
-  })
+				ToastService.success(UPDATE_PRODUCT_CATEGORY_SUCCESS_MESSAGE);
+			} catch (error) {
+				errorHandler(error, UPDATE_PRODUCT_CATEGORY_ERROR_MESSAGES);
+			}
+		},
+	});
 
-  const { isSubmitting } = useStore(updateCategoryForm.store)
+	const { isSubmitting } = useStore(updateCategoryForm.store);
 
-  async function handleSubmitLogin(event: SubmitEvent<HTMLFormElement>) {
-    event.preventDefault()
+	async function handleSubmitLogin(event: SubmitEvent<HTMLFormElement>) {
+		event.preventDefault();
 
-    await updateCategoryForm.handleSubmit()
-  }
+		await updateCategoryForm.handleSubmit();
+	}
 
-  return (
-    <>
-      <Toolbar
-        links={[
-          {
-            label: 'Lista de produtos',
-            link: APP_ROUTE.private.categories,
-          },
-        ]}
-        page="Editar categoria"
-      />
+	return (
+		<>
+			<Toolbar
+				links={[
+					{
+						label: "Lista de produtos",
+						link: APP_ROUTE.private.categories,
+					},
+				]}
+				page="Editar categoria"
+			/>
 
-      <Card className="w-full ">
-        <CardHeader>
-          <CardTitle>Editar categoria</CardTitle>
-        </CardHeader>
+			<Card className="w-full ">
+				<CardHeader>
+					<CardTitle>Editar categoria</CardTitle>
+				</CardHeader>
 
-        <CardContent>
-          <form onSubmit={handleSubmitLogin} className="flex flex-col gap-4">
-            <FormInput
-              name="category"
-              form={updateCategoryForm}
-              label="Nome da categoria"
-            />
+				<CardContent>
+					<form onSubmit={handleSubmitLogin} className="flex flex-col gap-4">
+						<FormInput
+							name="category"
+							form={updateCategoryForm}
+							label="Nome da categoria"
+						/>
 
-            <Button isLoading={isSubmitting} className="max-w-lg" type="submit">
-              Atualizar categoria
-            </Button>
-          </form>
-        </CardContent>
-      </Card>
-    </>
-  )
+						<FormSwitch
+							name="isActive"
+							label="Ativar categoria"
+							description="Se esta categoria estivar inativa todos os produtos relacionados a lena não estarão visíveis no catálogo"
+							form={updateCategoryForm}
+						/>
+
+						<Button isLoading={isSubmitting} className="max-w-lg" type="submit">
+							Atualizar categoria
+						</Button>
+					</form>
+				</CardContent>
+			</Card>
+		</>
+	);
 }
