ab0303a51168e02a528be6e24075213c33844bce
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-01 20:49:00 -0300

    feat: adiciona novas rotas

--- src/constants/api-routes.ts
+++ src/constants/api-routes.ts
@@ -1,30 +1,32 @@
 export const API_ROUTES = {
-  auth: {
-    login: '/api/auth/login',
-    logout: '/api/auth/logout',
-  },
-  productCategory: {
-    create: '/api/product-category',
-    findById: '/api/product-category/',
-    findAll: '/api/product-category',
-    delete: '/api/product-category/',
-    update: '/api/product-category/',
-  },
-  product: {
-    create: '/api/product',
-    delete: '/api/product/',
-    findById: '/api/product/',
-    update: '/api/product/',
-    setImages: '/api/product/images/',
-    findAll: '/api/product/list',
-    findByIdForAdmin: '/api/product/for-admin/',
-    findAllForAdmin: '/api/product/list/for-admin',
-  },
-  user: {
-    findById: '/api/user/',
-    delete: '/api/user/',
-    update: '/api/user/',
-    findAll: '/api/user/list',
-    create: '/api/user',
-  },
-} as const
+	auth: {
+		login: "/api/auth/login",
+		logout: "/api/auth/logout",
+	},
+	productCategory: {
+		create: "/api/product-category",
+		findById: "/api/product-category/",
+		findByIdForAdmin: "/api/product-category/for-admin/",
+		findAll: "/api/product-category/list",
+		findAllForAdmin: "/api/product-category/list/for-admin",
+		delete: "/api/product-category/",
+		update: "/api/product-category/",
+	},
+	product: {
+		create: "/api/product",
+		delete: "/api/product/",
+		findById: "/api/product/",
+		update: "/api/product/",
+		setImages: "/api/product/images/",
+		findAll: "/api/product/list",
+		findByIdForAdmin: "/api/product/for-admin/",
+		findAllForAdmin: "/api/product/list/for-admin",
+	},
+	user: {
+		findById: "/api/user/",
+		delete: "/api/user/",
+		update: "/api/user/",
+		findAll: "/api/user/list",
+		create: "/api/user",
+	},
+} as const;
