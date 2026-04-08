ee44697e15366b73b05e2ace0f2851aef584ef1f
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-07 21:33:22 -0300

    refactor: atualiza link para personalização

--- src/routes/(public)/login.tsx
+++ src/routes/(public)/login.tsx
@@ -1,14 +1,27 @@
+import { APP_ROUTE } from "@autoria/constants/app-route";
 import { buildPageHead } from "@autoria/libs/seo";
 import { LoginPage } from "@autoria/pages/login";
-import { createFileRoute } from "@tanstack/react-router";
+import { verifyAuthentication } from "@autoria/repositories/auth-repository";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(public)/login")({
-	head: () =>
-		buildPageHead({
-			title: "Login | Autoria",
-			description: "Area de acesso administrativo da Autoria.",
-			path: "/login",
-			robots: "noindex, nofollow",
-		}),
-	component: LoginPage,
+  head: () =>
+    buildPageHead({
+      title: "Login | Autoria",
+      description: "Area de acesso administrativo da Autoria.",
+      path: "/login",
+      robots: "noindex, nofollow",
+    }),
+  beforeLoad: async () => {
+    try {
+      await verifyAuthentication();
+    } catch {
+      return;
+    }
+
+    throw redirect({
+      to: APP_ROUTE.private.products,
+    });
+  },
+  component: LoginPage,
 });
